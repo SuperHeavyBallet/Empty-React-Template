6af446caf4b8ba7f74e04b04edf5cc066b0c5b33
Alexander Gorham <agram@hotmail.co.uk>
2024-03-03 05:12:06 +0100

    Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const helloWorld = document.createElement("h1");
+helloWorld.textContent = "Damn...";
+
+const root = document.getElementById("root");
+root.appendChild(helloWorld);
+
+/*
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+)*/
 
 ///
